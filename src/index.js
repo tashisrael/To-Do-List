07454b12f4864b356eaf9829889e35ba4f96993a
tashisrael <tashisrael04@gmail.com>
2022-09-29 15:44:53 +0300

    Implement add and remove functionality

--- src/index.js
+++ src/index.js
@@ -1,44 +1,29 @@
 import './style.css';
 
-const data = [
-  {
-    index: 1,
-    description: 'I will learn HTML',
-    completed: false,
-  },
-  {
-    index: 2,
-    description: 'I will learn CSS',
-    completed: false,
-  },
-  {
-    index: 3,
-    description: 'I will learn JavaScript',
-    completed: false,
-  },
-  {
-    index: 4,
-    description: 'I will learn WebPack',
-    completed: false,
-  },
-];
-const loadData = () => {
-  const sortedData = data.sort((a, b) => a.index - b.index);
-  const useData = sortedData.map((dat) => `
-    <div class="lists_li">
-      <div class="list_li_inputs" >
-        <input type="checkbox" name="" id="${dat.index}" ${dat.completed ? 'checked' : ''} value="${dat.completed}">
-        <label for="">${dat.description}</label>
-      </div>
-      <div class="make_dots">
-        <div class="dots"></div>
-        <div class="dots"></div>
-        <div class="dots"></div>
-      </div>
-    </div>
-  `);
+import Todolist, { addListItem } from './modules/script.js';
 
-  document.getElementById('list').innerHTML = useData.join('');
-};
+const enterKey = document.getElementById('enter-key');
 
-document.addEventListener('DOMContentLoaded', loadData());
+document.addEventListener('DOMContentLoaded', () => {
+  Todolist.createList();
+});
+
+addListItem.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    Todolist.addToList();
+    Todolist.createList();
+    addListItem.value = '';
+  }
+});
+
+enterKey.addEventListener('click', () => {
+  Todolist.addToList();
+  Todolist.createList();
+  document.querySelector('.input-text').value = '';
+});
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('to-do')) {
+    event.target.parentElement.classList.add('back');
+  }
+});
